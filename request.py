9d118a16dd2db720338d0373df0c304062e0e0a7
sunshay <sunshayina@outlook.com>
2022-07-14 23:06:17 -0700

    request

--- request.py
+++ request.py
@@ -3,8 +3,8 @@ import requests
 
 url = 'https://api.punkapi.com/v2/beers/random'
 
-r= requests.get(url)
+r= requests.get(url) # obtain json string 
 
-data = json.loads(r.text)
+data = json.loads(r.text) # we can't parse it with index directly,we need convert it to a dictionnary by json.loads first
 
-print(data[0]['name'])
+print(data[0]['name']) #parse it with index
