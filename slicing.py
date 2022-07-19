233440d9fbd556fe9a2d37b715cc951c15982dfa
sunshay <sunshayina@outlook.com>
2022-07-19 00:06:41 -0700

    add

--- slicing.py
+++ slicing.py
@@ -1,6 +1,12 @@
-def slicing(x):
-    return x[::-1]
+# def slicing(x):
+#     return x[::-1]
 
-slice = slicing('je suis la')
-print(slice)
+# slice = slicing('je suis la')
+# print(slice)
 
+a = 'attention a la corniche'
+# b = a[::-1]
+# print(b)
+b =[2,2,4,1]
+print(sorted(a))
+print(sorted(b))
